ddde085da0c6c9115e449449f2bcd038d4e0f39a
Arnav Gupta <arnav@codingblocks.com>
2020-03-27 20:04:15 +0530

    performance in js
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- Demo02/js-basics/todolist.js
+++ Demo02/js-basics/todolist.js
@@ -2,6 +2,18 @@ let btnAdd = document.getElementById('btnAdd')
 let newTask = document.getElementById('newTask')
 let taskList = document.getElementById('taskList')
 
-btnAdd.onclick = function () {
-    taskList.innerHTML += '<li>' + newTask.value + '</li>'
+btnAdd.onclick = function() {
+  const newItem = document.createElement('li')
+  newItem.textContent = newTask.value
+  taskList.appendChild(newItem)
+}
+
+function populateList() {
+  let start = Date.now()
+  for (let i = 0; i < 1000; i++) {
+    const newItem = document.createElement('li')
+    newItem.textContent = i
+    taskList.appendChild(newItem)
+  }
+  console.log('time taken =', Date.now() - start)
 }
