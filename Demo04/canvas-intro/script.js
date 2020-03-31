1db7d02873300853c204f05fa7100d384ad7e844
Arnav Gupta <arnav@codingblocks.com>
2020-03-31 22:08:26 +0530

    almost finished
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- Demo04/canvas-intro/script.js
+++ Demo04/canvas-intro/script.js
@@ -1,6 +1,10 @@
 let paintbox = document.getElementById('paintbox')
 let context = paintbox.getContext('2d')
 
+let gameOn = true
+
+let playerSpeed = 5
+
 class Box {
   constructor(size, color) {
     this.size = size
@@ -15,6 +19,10 @@ class Player extends Box {
     super(50, 'blue')
     this.x = 0
     this.y = 225
+    this.speed = 0
+  }
+  move() {
+    this.x += this.speed
   }
 }
 
@@ -25,37 +33,69 @@ class Enemy extends Box {
   }
 
   move() {
-      this.y += this.speed
-      if (this.y + this.size > 500) {
-          this.speed = -(Math.abs(this.speed))
-      }
-      if (this.y < 0) {
-          this.speed = Math.abs(this.speed)
-      }
+    this.y += this.speed
+    if (this.y + this.size > 500) {
+      this.speed = -Math.abs(this.speed)
+    }
+    if (this.y < 0) {
+      this.speed = Math.abs(this.speed)
+    }
   }
 }
 
 let player = new Player()
-let e1 = new Enemy(1)
-let e2 = new Enemy(2)
-e1.x = 120
-e2.x = 240
+let e1 = new Enemy(4)
+let e2 = new Enemy(8)
+let e3 = new Enemy(12)
+e1.x = 100
+e2.x = 233
+e3.x = 366
+
+function isCollided(box1, box2) {
+
+    // fill this yourself
+    
+}
 
 function drawBox(box) {
   context.fillStyle = box.color
   context.fillRect(box.x, box.y, box.size, box.size)
 }
 
-function updateGame() {
-  window.requestAnimationFrame(() => {
-    console.log('frame update')
-    context.clearRect(0, 0, 500, 500)
-    e1.move()
-    e2.move()
-    drawBox(e1)
-    drawBox(e2)
-    updateGame()
-  })
+paintbox.addEventListener('mousedown', () => {
+  player.speed = playerSpeed
+})
+
+paintbox.addEventListener('mouseup', () => {
+  player.speed = 0
+})
+
+setInterval(() => {
+    playerSpeed =  5 + parseInt(Math.random() * 10)
+    player.y = 100 + (Math.random() * 300)
+}, 2000)
+
+function gameLoop() {
+  if (!gameOn) return  
+  console.log('frame update')
+  context.clearRect(0, 0, 500, 500)
+  e1.move()
+  e2.move()
+  e3.move()
+  player.move()
+
+  if (isCollided(e1, player) || isCollided(e2, player) || isCollided(e3, player)) {
+    gameOn = false  
+    window.alert('Game Over')
+  }
+
+  drawBox(player)
+  drawBox(e1)
+  drawBox(e2)
+  drawBox(e3)
+
+
+  window.requestAnimationFrame(gameLoop)
 }
 
-updateGame()
+gameLoop()
