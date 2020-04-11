939e4a76f8194f7c953c679e78fb1d717c8231c1
Arnav Gupta <arnav@codingblocks.com>
2020-04-11 19:52:22 +0530

    major game functionality done
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- Demo06/tic-tac-toe/script.js
+++ Demo06/tic-tac-toe/script.js
@@ -9,18 +9,82 @@ const gameTable = document.getElementById('game')
 const game = {
   state: STARTED,
   turn: 'X',
+  move: 0
+}
+
+function endGame(winner) {
+    if (winner) {
+        alert('Game Over | Winner = ' + winner)
+    } else {
+        alert('Game Over | Draw')
+    }
+    game.state = ENDED
+}
+
+function restartGame() {
+    
 }
 
 function nextTurn() {
+    if (game.state === ENDED) return
+    
+    game.move++
     if (game.turn === 'X') game.turn = 'O'
     else game.turn = 'X'
+
+    if (game.move == 9) {
+        endGame()
+    }
 
     playerSpan.textContent = game.turn
 }
 
+function isSeqCaptured(arrayOf3Cells) {
+    let winnningCombo = game.turn + game.turn + game.turn
+    if (arrayOf3Cells.map(i => i.textContent).join('') === winnningCombo) {
+        endGame(game.turn)
+    }
+}
+
+function isRowCaptured(row) {
+    let tableRow = Array.from(gameTable.children[0].children[row - 1].children)
+    isSeqCaptured(tableRow)
+}
+function isColCaptured(col) {
+    let tableCol = [
+        gameTable.children[0].children[0].children[col - 1],
+        gameTable.children[0].children[1].children[col - 1],
+        gameTable.children[0].children[2].children[col - 1]
+    ]
+    isSeqCaptured(tableCol)
+}
+function isDiagCaptured(row, col) {
+    if (row !== col && (row + col) !== 4) return
+    let diag1 = [
+        gameTable.children[0].children[0].children[0],
+        gameTable.children[0].children[1].children[1],
+        gameTable.children[0].children[2].children[2]
+    ]
+    let diag2 = [
+        gameTable.children[0].children[0].children[2],
+        gameTable.children[0].children[1].children[1],
+        gameTable.children[0].children[2].children[0]
+    ]
+    isSeqCaptured(diag1)
+    isSeqCaptured(diag2)
+
+
+}
+
+
 function boxClicked(row, col) {
   console.log('box clicked = ', row, col)
 
+  let clickedBox = gameTable.children[0].children[row - 1].children[col - 1]
+  clickedBox.textContent = game.turn
+  isRowCaptured(row)
+  isColCaptured(col)
+  isDiagCaptured(row, col)
 
   nextTurn()
 }
