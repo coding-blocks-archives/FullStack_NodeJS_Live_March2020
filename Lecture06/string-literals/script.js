b7f927654dac511cda5b37c9acb53b1d42a462ba
Arnav Gupta <arnav@codingblocks.com>
2020-04-19 20:27:01 +0530

    string literals
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

---
+++ Lecture06/string-literals/script.js
@@ -0,0 +1,26 @@
+
+
+// He said, "This is is awesome!"
+
+let s1 = 'He said, "This is is awesome!"'
+
+let s2 = "He said, \"This is is awesome!\""
+
+let s3 = "He said, 'This is is awesome!'"
+
+let s4 = `He said, 'This is is awesome!'`
+
+let s5 = "This string is \n in multiple lines"
+
+let s6 = `
+    This string
+    is in multiple lines
+`
+
+let name = 'John'
+
+let g1 = 'Good Morning ' + name
+
+let g2 = `Good Morning ${name}`
+
+let s7 = `10 + 2 = ${10 + 2}`
