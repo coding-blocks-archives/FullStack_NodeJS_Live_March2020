82865e1fb88e41295c02b00eead7f27e390692a0
Arnav Gupta <arnav@codingblocks.com>
2020-04-19 19:41:22 +0530

    closures
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

---
+++ Lecture06/closure/script.js
@@ -0,0 +1,23 @@
+function outer(arg1) {
+  let var1 = 10
+  let x = 10
+  
+  function inner(arg2) {
+    let var2 = 20
+    let x = 20
+    console.log(arg1, var1, arg2, var2, x)
+    console.log(arguments[0])
+  }
+
+  return inner
+  // return 10
+}
+
+let x = outer('param1', 11)
+
+// typeof x == 'function'
+// x == inner
+
+// scope of arg1 and var1 are from lines 2 to 10
+
+x('param2')
