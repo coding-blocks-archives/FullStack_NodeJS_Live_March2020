6a5324dad0227a72dec4aecb27bed23b5b99f10c
Arnav Gupta <arnav@codingblocks.com>
2020-04-21 19:52:35 +0530

    prototypes
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

---
+++ Lecture07/prototypes/script.js
@@ -0,0 +1,43 @@
+
+let str = "Asdasd"                              // 3 levels from null
+let num = 233                                   // 3 levels from null
+let bool = true                                 // 3 levels from null
+let arr = [246,436,6,346]                       // 3 levels from null
+let obj = {a: 10, b: 'asdasd'}                  // 2 levels from null
+let fun = function () { console.log ('yay!') }  // 3 levels from null
+
+// if x and y are not primitive 
+// x == y : true <- what does this mean ? 
+// this means they are reference to the same object in memory 
+
+console.log('======= types =======')
+console.log('typeof String', typeof String)
+console.log('typeof Boolean', typeof Boolean)
+console.log('typeof Number', typeof Number)
+console.log('typeof Array', typeof Array)
+console.log('typeof Object', typeof Object)
+console.log('typeof Function', typeof Function)
+
+console.log('======= proto chain =======')
+console.log(str.__proto__.__proto__ == obj.__proto__)
+console.log(num.__proto__.__proto__ == obj.__proto__)
+console.log(bool.__proto__.__proto__ == obj.__proto__)
+console.log(arr.__proto__.__proto__ == obj.__proto__)
+console.log(fun.__proto__.__proto__ == obj.__proto__)
+
+// Everything indirectly inherits from the same thing
+// that obj is inherited from 
+// i.e. in Javascript, everything is essentially an Object
+
+console.log('======= prototypes ======= ')
+console.log(obj.__proto__ == Object.prototype)
+console.log(str.__proto__ == String.prototype)
+console.log(num.__proto__ == Number.prototype)
+console.log(bool.__proto__ == Boolean.prototype)
+console.log(arr.__proto__ == Array.prototype)
+console.log(fun.__proto__ == Function.prototype)
+
+// String.prototype inherits from Object.prototype
+
+// typeof Object.create(Boolean.prototype) -> ??
+
