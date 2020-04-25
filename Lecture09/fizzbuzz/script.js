3541c0c4d78941d74071cadbed8f4fe4eea3a2d1
Arnav Gupta <arnav@codingblocks.com>
2020-04-25 19:26:32 +0530

    update script.js
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

---
+++ Lecture09/fizzbuzz/script.js
@@ -0,0 +1,13 @@
+let inpNum = document.getElementById('inpNum')
+let btnPrint = document.getElementById('btnPrint')
+let ulNumlist = document.getElementById('ulNumlist')
+
+btnPrint.onclick = function () {
+
+    let start = Date.now()
+
+    // your code here
+
+    console.log('time taken', Date.now() - start)
+
+}
