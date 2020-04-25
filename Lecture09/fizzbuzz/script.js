e5dbae6c0e8a0389a2d5f191fb4d5548a4af2a13
Arnav Gupta <arnav@codingblocks.com>
2020-04-25 20:26:41 +0530

    fizzbuzz complete'
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- Lecture09/fizzbuzz/script.js
+++ Lecture09/fizzbuzz/script.js
@@ -7,12 +7,16 @@ btnPrint.onclick = function () {
     let start = Date.now()
     ulNumlist.innerHTML = ''
 
+    let c3 = 0
+    let c5 = 0;
     for (let i = 1; i <= inpNum.value; i++) {
         let li = document.createElement('li')
-        
+        c3++
+        c5++
+
         let print = ""
-        if (i % 3 == 0) print += "fizz"
-        if (i % 5 == 0) print += "buzz"
+        if (c3 == 3) { c3 = 0; print += "fizz" } 
+        if (c5 == 5) { c5 = 0; print += "buzz" }
         if (print === '') print = i
 
         li.textContent = print
