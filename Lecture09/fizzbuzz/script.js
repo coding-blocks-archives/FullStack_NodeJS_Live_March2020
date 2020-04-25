298463ba01ed4721d2b2dcd5cd1beb263a90c310
Arnav Gupta <arnav@codingblocks.com>
2020-04-25 20:08:24 +0530

    fizz buzz single mod
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- Lecture09/fizzbuzz/script.js
+++ Lecture09/fizzbuzz/script.js
@@ -5,9 +5,44 @@ let ulNumlist = document.getElementById('ulNumlist')
 btnPrint.onclick = function () {
 
     let start = Date.now()
+    ulNumlist.innerHTML = ''
 
-    // your code here
+    for (let i = 1; i <= inpNum.value; i++) {
+        let li = document.createElement('li')
+        
+        let print = ""
+        if (i % 3 == 0) print += "fizz"
+        if (i % 5 == 0) print += "buzz"
+        if (print === '') print = i
+
+        li.textContent = print
+        
+        
+        ulNumlist.appendChild(li)
+    }
 
     console.log('time taken', Date.now() - start)
 
 }
+
+/**
+ * Complexity of 
+ * a + b 
+ * if N = max num of bits in a and b 
+ * 
+ * +    = O(N)
+ * -    = O(N)
+ * *    = O(N^2)
+ * /    = O(N^2)
+ * %    = O(N^2)
+ * 
+ * 
+ * 3 + 4 
+ * 3 * 4 
+ * 9387693869 + 245764958
+ * 
+ * 
+ * x % 5 == 0 && x % 3 == 0
+ * x % 15 == 0 
+ * 
+ */
