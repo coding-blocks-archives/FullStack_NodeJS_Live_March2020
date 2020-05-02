1f212549533df7aa8271fc25aed0e6372c1e336b
Arnav Gupta <arnav@codingblocks.com>
2020-05-02 19:57:49 +0530

    for submissions
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- Lecture12/express-first-app/server.js
+++ Lecture12/express-first-app/server.js
@@ -3,7 +3,11 @@ const express = require('express')
 const app = express()
 
 app.get('/hello', (req, res) => {
-    res.send('Hello World')
+    let name = 'Guest'
+    
+    if (req.query.user) { name = req.query.user }
+    
+    res.send('Hello World ' + name)
 })
 
 app.use('/xyz', express.static(__dirname + '/public'))
