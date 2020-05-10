765307ec731dd7c75a239f7933910a12b489289b
Arnav Gupta <arnav@codingblocks.com>
2020-05-10 21:54:29 +0530

    get all posts and show on home
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- Lecture15/social-media-project/src/public/app/cbsocial-common.js
+++ Lecture15/social-media-project/src/public/app/cbsocial-common.js
@@ -1,20 +1,19 @@
 $(() => {
-
   $('#navbar').load('/components/navbar.html', loginIfNeeded)
   $('#footer').load('/components/footer.html')
-
-
+  $('#content').load('/components/all-posts.html', loadPosts)
 })
 
 function loginIfNeeded() {
-  window.currentUser = window.localStorage.user ? JSON.parse(window.localStorage.user) : null
+  window.currentUser = window.localStorage.user
+    ? JSON.parse(window.localStorage.user)
+    : null
   if (!currentUser) {
     $.post('/api/users', {}, (user) => {
       if (user) {
         console.log('registered current user as ', user.username)
         window.localStorage.user = JSON.stringify(user)
         currentUser = user
-        console.log($('#nav-username'))
         $('#nav-username').text(currentUser.username)
       }
     })
@@ -22,6 +21,31 @@ function loginIfNeeded() {
     console.log('resuming session as ', currentUser.username)
     console.log($('#nav-username'))
     $('#nav-username').text(currentUser.username)
-
   }
+}
+
+function loadPosts() {
+  $.get('/api/posts', (posts) => {
+    for (let p of posts) {
+      $('#posts-container').append(
+        $(`
+        <div class="col-4">
+          <div class="card m-2">
+            <div class="card-body">
+              <h5 class="card-title">${p.title}</h5>
+              <h6 class="card-subtitle mb-2 text-muted">${p.user.username}</h6>
+              <p class="card-text">
+                ${p.body.substr(0, 200)}
+                <a href="#">...read more</a>
+              </p>
+              <a href="#" class="card-link">Comment</a>
+              <a href="#" class="card-link">Like</a>
+            </div>
+          </div>
+        </div>
+        
+        `)
+      )
+    }
+  })
 }
