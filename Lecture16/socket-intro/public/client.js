49f10cfa5939a7d83b6e787d191d7842433a07cd
Arnav Gupta <arnav@codingblocks.com>
2020-05-14 20:28:10 +0530

    color the boxes (locally)
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- Lecture16/socket-intro/public/client.js
+++ Lecture16/socket-intro/public/client.js
@@ -5,3 +5,15 @@ let socket = io()
 socket.on('connect', () => {
   document.getElementById('socketId').innerText = socket.id
 })
+
+function colorTheBox(color) {
+  document.querySelector('.'+color).style.backgroundColor = color
+  setTimeout(() => {
+    document.querySelector('.'+color).style.backgroundColor = null
+  }, 500)
+}
+
+document.getElementById('colorIt').onclick = function () {
+  const color = document.getElementById('selectedColor').value
+  colorTheBox(color)
+}
