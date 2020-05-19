d783439a26d46e093cf24a0ebbaa4b9d47929474
Arnav Gupta <arnav@codingblocks.com>
2020-05-19 22:13:12 +0530

    finish profile and logout code
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- Lecture17/user-auth/server.js
+++ Lecture17/user-auth/server.js
@@ -12,7 +12,7 @@ app.use(express.json())
 app.use(session({
   resave: true,
   saveUninitialized: true,
-  secret: '24knb6k247b2k7b2k7bk247hb2kh7b2'
+  secret: '24knb6k247b2k7b2k7bk247hb2kh7b2',
 }))
 
 app.get('/signup', (req, res) => {
@@ -22,13 +22,42 @@ app.get('/signup', (req, res) => {
 app.post('/signup', async (req, res) => {
   const user = await Users.create({
     username: req.body.username,
-    password: req.body.password,
+    password: req.body.password, // NOTE: in production we save hash of password
     email: req.body.email
   })
 
   res.status(201).send(`User ${user.id} created`)
 })
 
+app.get('/login', (req, res) => {
+  res.render('login')
+})
+
+app.post('/login', async (req, res) => {
+  const user = await Users.findOne({where: { username: req.body.username }})
+  if (!user) {
+    return res.status(404).render('login', { error: 'No such username found' })
+  }
+
+  if (user.password !== req.body.password) {
+    return res.status(401).render('login', { error: 'Incorrect password' })
+  }
+  req.session.userId = user.id
+  res.redirect('/profile')  
+})
+
+app.get('/profile', async (req, res) => {
+  if (!req.session.userId) {
+    return res.redirect('/login')
+  }
+  const user = await Users.findByPk(req.session.userId)
+  res.render('profile', { user })
+})
+
+app.get('/logout', (req, res) => {
+  req.session.userId = null
+  res.redirect('/login')
+})
 
 db.sync()
   .then(() => {
